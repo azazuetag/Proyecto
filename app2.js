89bcf3c3226cab2e1120c6b47759eb69e07191f2
Alejandro Guzmán Zazueta <alguzman@itroque.edu.mx>
2018-03-15 11:00:02 -0600

    Eliminando los .old

--- app2.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var app = express();
-
-app.use(express.static("./"));
-
-app.listen(8000,function(){
-	console.log("Esta corriendo el servidor en el puerto 8000");
-});
